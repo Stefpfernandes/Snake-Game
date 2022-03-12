c178001e46582adc736a0a9c1a9679ba44abe529
Stefanie Pereira Fernandes <stefaniepereirafernandes@gmail.com>
2022-03-11 21:56:17 -0300

    Adicionado Cronômetro

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ let food = {
     y: Math.floor(Math.random() * 15 + 1) * box
 }
 
+window.onload = function(){
+    start();
+}
+
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -49,7 +53,9 @@ function iniciarJogo() {
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
+            stop();
             alert('Game Over :(');
+            location.reload();
         }
     }
 
@@ -78,8 +84,50 @@ function iniciarJogo() {
     }
 
     snake.unshift(newHead);
+}
 
+/*CRONÔMETRO*/
+"use strict"
+
+var hh = 0;
+var mm = 0;
+var ss = 0;
+
+var tempo = 1000; //qtd milésimos valem 1 segundo
+var cron;
+
+function start(){
+    cron = setInterval(() => {timer();}, tempo);
+}
+
+function pause(){
+    clearInterval(cron);
+}
+
+function stop(){
+    clearInterval(cron);
+    hh = 0;
+    mm = 0;
+    ss = 0;
+
+    document.getElementById('counter').innerText = '00:00:00';
+}
+
+function timer(){
+    ss++;
+
+    if(ss == 60){
+        ss = 0;
+        mm++;
+
+        if(mm == 60){
+            mm = 0;
+            hh++;
+        }
+    }
+
+    var format = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
+    document.getElementById('counter').innerText = format;
 }
 
 let jogo = setInterval(iniciarJogo, 100);
-
